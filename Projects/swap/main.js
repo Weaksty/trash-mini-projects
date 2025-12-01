4d9683cac046ba1c2a339fd1ec43f7d0f55ccf45
weaksty <rizkoarik9@gmail.com>
2025-12-02 00:01:38 +0100

    bug fix

--- Projects/swap/main.js
+++ Projects/swap/main.js
@@ -40,3 +40,69 @@ function All(){
 
 }
 
+
+
+let pos = 0
+let width = 300
+let speed = 4
+let totalWidth = 3000 + 160
+let move = 0 // if 1 move left if -1 move rigth if 0 stop
+
+const img = document.querySelectorAll("img")
+const track = document.querySelector('.track')
+const left = document.querySelector('.left')
+const rigth = document.querySelector('.rigth')
+
+
+
+function amimate(){
+    if(move!== 0){
+        pos += move * speed
+    }
+    if(pos >    0){
+        pos = -2470
+    }
+    if(pos < -3110){
+        pos = -640
+    }
+    
+track.style.transform = `translateX(${pos}px)`
+
+requestAnimationFrame(amimate);
+
+}
+
+amimate()
+left.addEventListener("mouseenter", ()=> {
+    move = 1
+})
+rigth.addEventListener("mouseenter", ()=> {
+    move = -1
+})
+left.addEventListener("mouseleave", ()=> {
+    move = 0
+})
+rigth.addEventListener("mouseleave", ()=> {
+    move = 0
+})
+img.forEach(e => {
+    e.addEventListener("mouseenter", ()=> {
+    left.style.backgroundColor = "rgba(255, 255, 255, 0)"
+})
+});  
+img.forEach(e => {
+    e.addEventListener("mouseenter", ()=> {
+    rigth.style.backgroundColor = "rgba(255, 255, 255, 0)"
+})
+}); 
+img.forEach(e => {
+    e.addEventListener("mouseleave", ()=> {
+    left.style.backgroundColor = "rgba(255, 255, 255, 0.2)"
+})
+}); 
+img.forEach(e => {
+    e.addEventListener("mouseleave", ()=> {
+    rigth.style.backgroundColor = "rgba(255, 255, 255, 0.2)"
+})
+}); 
+
